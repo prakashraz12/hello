e6bce847d00235a5edc88c8f7a603c97a796b2d2
praksh raz <rzprakash16@gmail.com>
2022-11-16 10:14:19 +0545

    Initial commit

---
+++ frontend/pages/app.js
@@ -0,0 +1,16 @@
+const submitBtn = document.getElementById('submit');
+
+
+const inputName = document.getElementById('name-input').value
+
+const inputQuanity = document.getElementById('input-q').value
+
+const price = document.getElementById('price').value
+
+
+const dateShow = document.getElementById('date-1')
+const itemName = doocument.getElementById('itemName')
+
+submitBtn.addEventListener('click', function(){
+	console.log('hey');
+})
